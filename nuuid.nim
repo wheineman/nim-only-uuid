d91d0dc13f2d4def5890fc517b0bca8abe9d4d31
U-AMER\HeinemanWP <HeinemanWP@AMRLR9004L63.amer.pfizer.com>
2015-05-13 18:02:20 -0400

    Change name from uuid to nuuid

---
+++ nuuid.nim
@@ -0,0 +1,23 @@
+##
+## uuid.nim
+##
+##
+
+import math
+import sequtils
+import strutils
+import times
+
+randomize()
+
+proc getTime(): int  =
+  toInt(epochTime() * 100000)
+
+proc generateUUID*(): string =
+  var d = getTime()
+  var pattern = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+  proc fn(c : char): string =
+    var r = toInt(toFloat(d) + random(1.0) * 16) %% 16
+    d = toInt(floor(toFloat(d) / 16))
+    toHex(if c == 'x': r else: r and 0x3 or 0x8, 1)
+  toLower(join(pattern.mapIt(string, if it == 'x' or it == 'y': fn(it) else: $it)))
---
+++ nuuid.nimble
@@ -0,0 +1,9 @@
+[Package]
+name          = "nuuid"
+version       = "0.1.0"
+author        = "Willy Heineman"
+description   = "A Nim only UUID generator"
+license       = "MIT"
+
+[Deps]
+Requires: "nim >= 0.10.0"
